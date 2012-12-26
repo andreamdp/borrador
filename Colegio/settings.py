0563f96baefff511e13dc3c88ac7410fe9c2735d
gebelein <gebeleinmdq@gmail.com>
2012-12-26 15:57:07 -0200

    my django app

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -13,11 +13,11 @@ MANAGERS = ADMINS
 #GRAPPELLI_ADMIN_TITLE = 'Residencias - Colmed IX'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'DBColegio',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -172,5 +172,5 @@ LOGGING = {
     }
 }
 
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
