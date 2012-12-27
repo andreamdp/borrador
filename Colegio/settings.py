cdc99aeee263259c70e9c02eb9592e34c1be5460
gebelein <gebeleinmdq@gmail.com>
2012-12-27 12:02:57 -0200

    q

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 #GRAPPELLI_ADMIN_TITLE = 'Residencias - Colmed IX'
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',#Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
