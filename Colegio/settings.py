d8b4dd3b0999796d2f4a1a889b5262983bc43d63
gebelein <gebeleinmdq@gmail.com>
2012-12-27 11:27:16 -0200

    q

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': ''#Or path to database file if using sqlite3.
+        'NAME': '',#Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
