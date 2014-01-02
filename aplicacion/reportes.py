d8f977b44d52c455ddd6cf88d6983474d19eeb9f
andrea <andrea@andrea-VirtualBox.(none)>
2014-01-02 19:38:33 -0300

    pruebo suma

--- aplicacion/reportes.py
+++ aplicacion/reportes.py
@@ -57,6 +57,8 @@ class ReportGrupoInst(Report):
                     ObjectValue(attribute_name='Institucion', left=1, top=0.1*cm, width=20*cm,
                         get_value=lambda instance: 'Institucion: ' + (instance.institucion.nombre),
                         style={'fontName': 'Helvetica-Bold', 'fontSize': 12}),
+                    ObjectValue(attribute_name='id', action=FIELD_ACTION_COUNT,
+                       display_format='%s staffs', left=0.5*cm, top=0.1*cm)
                    # ObjectValue(attribute_name='especialidad', action=FIELD_ACTION_COUNT,
                    #     display_format='%s ', left=15*cm, top=0.1*cm,style={'fontName': 'Helvetica-Bold', 'fontSize': 12})
                    
