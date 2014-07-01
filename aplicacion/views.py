f2bcb1175931a1983f48a16436d165459aaf23e8
andrea <andrea@andrea-VirtualBox.(none)>
2014-07-01 19:51:06 -0300

    saco de views add residente y lo paso a vResidente.py

--- aplicacion/views.py
+++ aplicacion/views.py
@@ -18,7 +18,6 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import Http404
 from aplicacion.reportes import *
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response, redirect, get_object_or_404
 
 def edit(request, pk):
@@ -179,84 +178,5 @@ def grupos(request,residenciaaut_a_Comienzo):
 
     return response
 
-def residente_list(request,residenciaaut_id,template_name='aplicacion/residente/list.html' ):
-    residentes_list = Residente.active.filter(residencia_id=residenciaaut_id).order_by('tipoR')
-    paginator = Paginator(residentes_list,  6)
-    page = request.GET.get('page')
-    try:
-        residentes = paginator.page(page)
-    except PageNotAnInteger:
-        residentes = paginator.page(1)
-    except EmptyPage:
-        residentes = paginator.page(paginator.num_pages)
-    return render_to_response(template_name, {
-        'residentes': residentes,
-        'idR:': request.user.is_authenticated()
-      #  'paginator': paginator,
-      #  'page': page,
-    })
-from django.shortcuts import redirect
-from django import template
-register = template.Library()  
-@csrf_protect
-@register.inclusion_tag('aplicacion/residente/add.html', takes_context=True)
-def residente_add1(request, residenciaaut_id,form_class=ResidenteForm1, template_name='aplicacion/residente/add.html'):
-    if request.POST:
-        form = form_class(request.POST)
-        if form.is_valid():
-            residente = form.save()
-          
-            return redirect('residente_list',residenciaaut_id)
-            
-    else:
-       form = ResidenteForm1(initial={'residencia': residenciaaut_id})
-      
-    return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))
-
-
-@csrf_protect
-#@register.inclusion_tag('aplicacion/residente/add.html', takes_context=True)
-def residente_add2(request, residenciaaut_id,form_class=ResidenteForm2, template_name='aplicacion/residente/add.html'):
-    if request.POST:
-        form = form_class(request.POST)
-        if form.is_valid():
-            residente = form.save()
-            return redirect('residente_list',residenciaaut_id)
-    else:
-       form = ResidenteForm2(initial={'residencia': residenciaaut_id})
-    return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))   
-@csrf_protect
-
-def residente_add3(request, residenciaaut_id,form_class=ResidenteForm3, template_name='aplicacion/residente/add.html'):
-    if request.POST:
-        form = form_class(request.POST)
-        if form.is_valid():
-            residente = form.save()
-            return redirect('residente_list',residenciaaut_id)
-    else:
-       form = ResidenteForm3(initial={'residencia': residenciaaut_id})
-    return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))
-@csrf_protect
-
-def residente_add4(request, residenciaaut_id,form_class=ResidenteForm4, template_name='aplicacion/residente/add.html'):
-    if request.POST:
-        form = form_class(request.POST)
-        if form.is_valid():
-            residente = form.save()
-            return redirect('residente_list',residenciaaut_id)
-    else:
-       form = ResidenteForm4(initial={'residencia': residenciaaut_id})
-    return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))
-
-@csrf_protect
-def jefeResidente_add(request, residenciaaut_id,form_class=JefeResidenteForm, template_name='aplicacion/residente/add.html'):
-    if request.POST:
-        form = form_class(request.POST)
-        if form.is_valid():
-            residente = form.save()
-            return redirect('residente_list',residenciaaut_id)
-    else:
-       form = JefeResidenteForm(initial={'residencia': residenciaaut_id})
-    return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))   
 
   
