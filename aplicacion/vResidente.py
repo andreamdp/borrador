52ccbfa95110dda15a7dcaa2336affd5149c9efc
andrea <andrea@andrea-VirtualBox.(none)>
2014-07-01 20:21:21 -0300

    modificacion en vResidente.py

--- aplicacion/vResidente.py
+++ aplicacion/vResidente.py
@@ -25,7 +25,17 @@ def residente_list(request,residenciaaut_id,template_name='aplicacion/residente/
       #  'paginator': paginator,
       #  'page': page,
     })
-    
+def residente_edit(request,residenciaaut_id, residente_id):
+    residente = get_object_or_404(Residente, pk=residente_id)
+    form = ResidenteForm1(request.POST or None, instance=residente)
+    if form.is_valid():
+        residente = form.save()
+        return redirect('/'+residenciaaut_id+'/lista/')
+
+    return render_to_response('aplicacion/residente/edit.html',
+                              {'form': form,
+                               'residente_id': residente_id},
+                              context_instance=RequestContext(request))    
 def residente_delete(request,residenciaaut_id, residente_id):
     residente = get_object_or_404(Residente, pk=residente_id)
     residente.delete()
