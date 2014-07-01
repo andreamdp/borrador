de1f44416024ba97c0ff751aba9da03fc8b3b95c
andrea <andrea@andrea-VirtualBox.(none)>
2014-07-01 20:15:40 -0300

    agrego edit y add de los residentes de 2,3,4 año y jefes de residencias

--- Colegio/urls.py
+++ Colegio/urls.py
@@ -5,6 +5,7 @@ from aplicacion.views import *
 from aplicacion.vFicha import *
 from aplicacion.forms import *
 from aplicacion.vResidente import *
+from aplicacion.vResidenteEdit import *
 #from django.views.generic import list_detail, date_based, create_update
 
 admin.autodiscover()
@@ -22,16 +23,17 @@ urlpatterns = patterns('',
     (r'reporte/2013/$', residencias2013),
     (r'reporte/2014/$', residencias2014),
     (r'^grupoInst/(?P<residenciaaut_a_Comienzo>\d+)$',grupos),
-    (r'(?P<residenciaaut_id>\d+)/residente1/$', residente_add1),
-#    (r'(?P<residenciaaut_id>\d+)/residente2/$', residente_add2),
-#    (r'(?P<residenciaaut_id>\d+)/residente3/$', residente_add3),
-#    (r'(?P<residenciaaut_id>\d+)/residente4/$', residente_add4),
+    (r'(?P<residenciaaut_id>\d+)/residente1/$', residente1_add),
+    (r'(?P<residenciaaut_id>\d+)/residente2/$', residente2_add),
+    (r'(?P<residenciaaut_id>\d+)/residente3/$', residente3_add),
+    (r'(?P<residenciaaut_id>\d+)/residente4/$', residente4_add),
+    (r'(?P<residenciaaut_id>\d+)/residente4/$', residente4_add),
     (r'^(?P<residenciaaut_id>\d+)/lista/residente1_edit/(?P<residente_id>\d+)/$', residente1_edit),
- #   url(r'^residente2_edit/(?P<residente_id>\d+)/$', residente1_edit),
- #   url(r'^residente3_edit/(?P<residente_id>\d+)/$', residente1_edit),
- #   url(r'^residente4_edit/(?P<residente_id>\d+)/$', residente1_edit),
- #   url(r'^residente5_edit/(?P<residente_id>\d+)/$', residente1_edit),
-    url(r'^(?P<residenciaaut_id>\d+)/lista/residente1_delete/(?P<residente_id>\d+)/$',residente1_delete),
+    (r'^(?P<residenciaaut_id>\d+)/lista/residente2_edit/(?P<residente_id>\d+)/$', residente2_edit),
+    (r'^(?P<residenciaaut_id>\d+)/lista/residente3_edit/(?P<residente_id>\d+)/$', residente3_edit),
+    (r'^(?P<residenciaaut_id>\d+)/lista/residente4_edit/(?P<residente_id>\d+)/$', residente4_edit),
+    (r'^(?P<residenciaaut_id>\d+)/lista/jefeResidente_edit/(?P<residente_id>\d+)/$', jefeResidente_edit),
+    url(r'^(?P<residenciaaut_id>\d+)/lista/residente_delete/(?P<residente_id>\d+)/$',residente_delete),
     url(r'(?P<residenciaaut_id>\d+)/lista/$', residente_list,name='residente_list'),
     
     url('', include(admin.site.urls)),
--- aplicacion/vResidente.py
+++ aplicacion/vResidente.py
@@ -25,28 +25,27 @@ def residente_list(request,residenciaaut_id,template_name='aplicacion/residente/
       #  'paginator': paginator,
       #  'page': page,
     })
-
-
-
+    
+def residente_delete(request,residenciaaut_id, residente_id):
+    residente = get_object_or_404(Residente, pk=residente_id)
+    residente.delete()
+    return redirect('/'+residenciaaut_id+'/lista/')
 
 @csrf_protect
 @register.inclusion_tag('aplicacion/residente/add.html', takes_context=True)
-def residente_add1(request, residenciaaut_id,form_class=ResidenteForm1, template_name='aplicacion/residente/add.html'):
+def residente1_add(request, residenciaaut_id,form_class=ResidenteForm1, template_name='aplicacion/residente/add.html'):
     if request.POST:
         form = form_class(request.POST)
         if form.is_valid():
             residente = form.save()
-          
             return redirect('residente_list',residenciaaut_id)
-            
     else:
-       form = ResidenteForm1(initial={'residencia': residenciaaut_id})
-      
+       form = ResidenteForm1(initial={'residencia': residenciaaut_id})  
     return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))
 
 @csrf_protect
 #@register.inclusion_tag('aplicacion/residente/add.html', takes_context=True)
-def residente_add2(request, residenciaaut_id,form_class=ResidenteForm2, template_name='aplicacion/residente/add.html'):
+def residente2_add(request, residenciaaut_id,form_class=ResidenteForm2, template_name='aplicacion/residente/add.html'):
     if request.POST:
         form = form_class(request.POST)
         if form.is_valid():
@@ -57,7 +56,7 @@ def residente_add2(request, residenciaaut_id,form_class=ResidenteForm2, template
     return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))   
 @csrf_protect
 
-def residente_add3(request, residenciaaut_id,form_class=ResidenteForm3, template_name='aplicacion/residente/add.html'):
+def residente3_add(request, residenciaaut_id,form_class=ResidenteForm3, template_name='aplicacion/residente/add.html'):
     if request.POST:
         form = form_class(request.POST)
         if form.is_valid():
@@ -68,7 +67,7 @@ def residente_add3(request, residenciaaut_id,form_class=ResidenteForm3, template
     return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))
 @csrf_protect
 
-def residente_add4(request, residenciaaut_id,form_class=ResidenteForm4, template_name='aplicacion/residente/add.html'):
+def residente4_add(request, residenciaaut_id,form_class=ResidenteForm4, template_name='aplicacion/residente/add.html'):
     if request.POST:
         form = form_class(request.POST)
         if form.is_valid():
@@ -89,20 +88,4 @@ def jefeResidente_add(request, residenciaaut_id,form_class=JefeResidenteForm, te
        form = JefeResidenteForm(initial={'residencia': residenciaaut_id})
     return render_to_response(template_name,{'form':form,'idResidencia':residenciaaut_id},context_instance=RequestContext(request))   
 
-def residente1_edit(request,residenciaaut_id, residente_id):
-    residente = get_object_or_404(Residente, pk=residente_id)
-    form = ResidenteForm1(request.POST or None, instance=residente)
-    if form.is_valid():
-        residente = form.save()
-       # residente.save()
-        return redirect('/'+residenciaaut_id+'/lista/')
 
-    return render_to_response('aplicacion/residente/edit.html',
-                              {'form': form,
-                               'residente_id': residente_id},
-                              context_instance=RequestContext(request)) 
-    
-def residente1_delete(request,residenciaaut_id, residente_id):
-    residente = get_object_or_404(Residente, pk=residente_id)
-    residente.delete()
-    return redirect('/'+residenciaaut_id+'/lista/')
