f6de9f63ef652a3db41c93b5dd3e23410664ad03
andrea <andrea@andrea-VirtualBox.(none)>
2014-07-01 16:12:54 -0300

    settings con carpeta static

--- Colegio/settings.py
+++ Colegio/settings.py
@@ -59,11 +59,11 @@ ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/grappelli/static/'
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/grappelli/static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
